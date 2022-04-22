89143098591ba1e7597a91f52dde02b0a914cd15
Adel Absalyamov <adel.absalyamov@yandex.ru>
2022-04-22 14:52:29 +0300

    fix: style index

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ function App() {
     <div className="wrapper clear">
       <Drawer />
       <Header />
-      <div className='content p-40'>
-            <div className='d-flex align-center justify-between mb-40'>
-            <h1>Все кросовки</h1>
-            <div className='search-block'>
-                <img src="/img/search.svg" alt="Search" />
-                <input placeholder='Поиск...' />
-                </div>
-            </div>
-            <div className='d-flex'>
-            <Card />
-            </div>
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кросовки</h1>
+          <div className="search-block">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
         </div>
+        <div className="d-flex">
+          <Card />
+        </div>
+      </div>
     </div>
   );
 }
